5bf58ab73eb89f1d9cda5c28c0c4987450b4d4b6
AlyssaU3 <alys4321c@gmail.com>
2025-09-26 05:06:57 -0400

    23 done

--- src/red_green_blue.py
+++ src/red_green_blue.py
@@ -4,12 +4,27 @@ import re
 
 def red_green_blue(filename="src/rgb.txt"):
     
+    with open(filename, "r") as a:
 
+        final=[]
+        
+        for line in a.readlines():
+            
+            
+                    colors=re.match(r"^\s*(\d+)\s+(\d+)\s+(\d+)[ \t]+(.+)$", line)
+               #print(colors)
+                    if colors:
+                        red, green, blue, name = colors.groups()
+                        final.append(f"{red}\t{green}\t{blue}\t{name}")
+        return(final)
+
+    
 
 
 def main():
-    result = red_green_blue()
-    print()
+     result = red_green_blue()
+     print(result)
+    #red_green_blue()
 
     #print(f"The result should be a list, it is a {type(result)}
     #print(f"The length of the list should be 753, was {len(result)}   
